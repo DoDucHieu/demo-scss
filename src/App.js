88bf1548b90487b818493858aa0e002360fb2bf5
DoDucHieu <hieuncncnc@gmail.com>
2023-07-04 16:46:26 +0700

    demo_scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">header</div>
+            <div className="body">
+                <div className="list-btn">
+                    <button className="btn-cancel">Cancel</button>
+                    <button className="btn-edit">Edit</button>
+                    <button className="btn-create">Create</button>
+                    <button className="btn-del">Delete</button>
+                </div>
+            </div>
+            <div className="footer">footer</div>
+        </div>
+    );
 }
 
 export default App;
